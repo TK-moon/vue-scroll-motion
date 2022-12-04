cbc63d82b071bd6eda68966a85dd16826c3001e7
TK-moon <tkmoon.dev@gmail.com>
2022-12-04 18:31:11 +0900

    :wrench: fix d.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,2 @@
-// import type { Plugin } from "vue"
-
-// import components from "./components"
-// export default components
-
-// export const VaguerComponents: Plugin = {
-//   install(app) {
-//     Object.entries(components).forEach(([key, value]: [string, any]) => {
-//       app.component(key, value)
-//     })
-//   },
-// }
-
-// START_EXPORTS
 export { default as AnimationSection } from "./components/AnimationSection.vue"
 export { default as Animator } from "./components/Animator.vue"
-// END_EXPORTS
