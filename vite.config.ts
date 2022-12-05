a965201def4fc0b980b300f04f21f82dffead1fe
TK-moon <tkmoon.dev@gmail.com>
2022-12-05 22:37:56 +0900

    :wrench: update vite.config.js for css inject

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import dts from "vite-plugin-dts"
+import VitePluginStyleInject from "vite-plugin-style-inject"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts({ insertTypesEntry: true })],
+  plugins: [vue(), vueJsx(), dts({ insertTypesEntry: true }), VitePluginStyleInject()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
