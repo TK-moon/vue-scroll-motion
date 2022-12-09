d63eec53c79c7226e226a5d16aa0ab5efd01834a
TK-moon <tkmoon.dev@gmail.com>
2022-12-10 00:17:40 +0900

    :hammer: demo page build

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -3,12 +3,10 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
-import dts from "vite-plugin-dts"
-import VitePluginStyleInject from "vite-plugin-style-inject"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts({ insertTypesEntry: true }), VitePluginStyleInject()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -17,5 +15,6 @@ export default defineConfig({
   base: "",
   build: {
     outDir: "demo",
+    minify: "esbuild",
   },
 })
